e66cdea7a03495554471f186515e0030ffad5e34
S Meher Siva Ram <smsram001@gmail.com>
2025-02-26 21:14:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -104,4 +104,4 @@ def summarize_video():
     return jsonify({"subtitles": subtitles, "summary": summary})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8000)
